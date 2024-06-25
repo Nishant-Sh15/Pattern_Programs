22f9824ee03b7c0142b42362f72f9e4bde6f8fe9
Nishant <sh.nishant15j5@gmail.com>
2024-06-26 01:07:40 +0530

    butterfly_pattern added comments

--- butterfly_pattern.c
+++ butterfly_pattern.c
@@ -1,15 +1,23 @@
+//adding header files
 #include<stdio.h>
 #include<conio.h>
 
+// main function 
+
 void main(){
-    int height,row=1,nspace,nstar,i;
+    // defining variables
+    int height,row=1,nspace,i;     //nspace represents number of times space will be printed in a row
 
     clrscr();
 
     printf("enter height of butterfly : ");
+
+    // getting value of height i.e number of rows the butterfly will be containing
     scanf("%d",&height);
 
     nspace=height-2;
+
+    // upper half of butterfly
     while(nspace>-1){
         for(i=1;i<=(height-nspace)/2;i++)
             printf("* ");
@@ -24,11 +32,14 @@ void main(){
             nspace-=2;
         
     }
+
+    // resetting value of nspace based the fact that whether height is an even number or odd
     if(height%2 != 0)
         nspace=1;
     else
         nspace=0;
 
+    // lower half of the butterfly
     while(nspace<=height-2){
         
         for(i=1;i<=(height-nspace)/2;i++)
@@ -41,5 +52,6 @@ void main(){
         nspace+=2;
 
     }
+    // delaying the stoping of execution until some key is pressed
     getch();
 }
