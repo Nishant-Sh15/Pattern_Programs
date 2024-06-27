c5d9d214d5e073e62200695ac0abcb207a54d831
Nishant <sh.nishant15j5@gmail.com>
2024-06-27 12:25:32 +0530

    added comments and renames file of q4

--- q4_hash_numbers.c
+++ q4_hash_numbers.c
@@ -1,8 +1,16 @@
+// including header files
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
+
+// defining main function
 void main(){
+    // defining variables 
+    // *narr is a pointer which we will be using as a pointer to an array in this programm using dynamic memory allocation
+    // temp is variable temporarily store the value of number
     int i,num,*narr,temp,rem,j,k,length;
+
+    // arr is a three dimensional array containing strings which are rows of a number when they are printed
     char arr [10][8][4]={
         {"####","#  #","#  #","#  #","#  #","#  #","#  #","####"},
         {"  # "," ## ","# # ","  # ","  # ","  # ","  # ","####"},
@@ -15,11 +23,17 @@ void main(){
         {"####","#  #","#  #","####","#  #","#  #","#  #","####"},
         {"####","#  #","#  #","####","   #","   #","   #","   #"}};
 
+    // clearing the screen
     clrscr();
 
+    // asking for number i.e num
     printf("enter any number less than five digit");
+    
     scanf("%d",&num);
+
     temp=num;
+
+    // finding lenth of number entered
     for(length=0;temp!=0;length++){
     temp=temp/10;
     }
@@ -29,14 +43,17 @@ void main(){
     rem=temp%10;
     temp=temp/10;
 
+    // forming a one dimensional  array using dynamic memory allocation
     narr= (int*)calloc(length*sizeof(1),sizeof(1));
 
+    // storing each digit of number in number array i.e narr
     for(i=length-1;i>-1;i--){
         narr[i]=rem;
         rem=temp%10;
         temp=temp/10;
     }
 
+    // printing the number entered in form of hash patterns
     for(i=0;i<8;i++){
         for(j=0;j< length;j++){
             for(k=0;k<4;k++){
@@ -47,8 +64,7 @@ void main(){
         printf("\n");
     }
 
+    // holding the stopping of programm's execution until some key is pressed
     getch();
-
-    
-
+ 
 }
