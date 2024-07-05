016a0194d79d73ed7f8dbd29b18532b09323ddbf
Nishant <sh.nishant15j5@gmail.com>
2024-07-05 12:26:26 +0530

    added comments in q5

--- q5_number_square.c
+++ q5_number_square.c
@@ -1,23 +1,33 @@
+// including header files
 #include <stdio.h>
 #include<conio.h>
 #include<stdlib.h>
 
+
+// defining main function
 void main(){
+    // defining variables
     int n,flag=0,**arr,rmin,cmin,r,c,rmax,cmax,i;
     
+    // clearing the screen
     clrscr();
     
+    // asking for the number
     printf("enter a number");
-    
+
+    // storing the number in variable "n"
     scanf("%d",&n);
     
+    // configuiring the initial values of minimum and maximum row and column
     rmin=0;
     cmin=0;
     rmax=n-1;
     cmax=n-1;
     
+    // creating a pointer for two dimensional array of size required n*n natural numbers
     arr=(int**)calloc(n*sizeof(1),sizeof(n));
     
+    // storing values of each one dimensional array 
     for(i=0;i<n;i++)
         *(arr+i)=(int*)calloc(n*sizeof(1),sizeof(1));
     
@@ -75,7 +85,8 @@ void main(){
         
         if(flag==1)
             break;
-            
+         
+        // updating values of minimum and maximum value of rows and columns after a complete cycle   
         rmin++;
         cmin++;
         rmax--;
@@ -84,12 +95,14 @@ void main(){
         
     }
     
+    // printing the nuber square stored in the array
     for(r=0;r<n;r++){
         for(c=0;c<n;c++)
             printf("%5d  ",*(*(arr+r)+c));
         printf("\n");
     }
     
+    // preventing the execution from stopping  until a key is pressed
     getch();
 
 }
